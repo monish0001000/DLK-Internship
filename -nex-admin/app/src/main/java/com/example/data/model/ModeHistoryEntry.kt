c07877374d85d6528332c965c29435823055b03d
hemalatha <hema91041@gmail.com>
2026-07-14 19:17:34 +0530

    Delete -nex-admin directory

--- -nex-admin/app/src/main/java/com/example/data/model/ModeHistoryEntry.kt
+++
@@ -1,9 +0,0 @@
-package com.example.data.model
-
-import com.google.firebase.database.IgnoreExtraProperties
-
-@IgnoreExtraProperties
-data class ModeHistoryEntry(
-    val mode: String = "",
-    val changed_at: String = ""
-)
--- -nex-admin/app/src/main/java/com/example/ui/login/LoginState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.ui.login
-
-sealed class LoginState {
-    object Idle : LoginState()
-    object Loading : LoginState()
-    data class Success(val userId: String) : LoginState()
-    data class Error(val message: String) : LoginState()
-}
